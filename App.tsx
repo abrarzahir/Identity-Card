29390cb064104bf595507ef208b42ebe623b895a
abrar niloy <abrar.zahir.niloy100@gmail.com>
2024-09-23 14:53:55 +0600

    updated

--- App.tsx
+++ App.tsx
@@ -1,9 +1,28 @@
-//import liraries
-import React, { Component } from 'react';
+// Import libraries
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import SplashScreen from './SplashScreen';
 
-// create a component
+// Create the main component
 const App = () => {
+  const [isSplashVisible, setIsSplashVisible] = useState(true);
+
+  // Use useEffect to handle the splash screen timeout
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsSplashVisible(false);
+    }, 3000); // 3 seconds
+
+    // Clear the timer when the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+
+  // If the splash screen is visible, return the SplashScreen component
+  if (isSplashVisible) {
+    return <SplashScreen />;
+  }
+
+  // Otherwise, return the main app component
   return (
     <View style={styles.container}>
       <Text>MyComponent</Text>
@@ -11,15 +30,14 @@ const App = () => {
   );
 };
 
-// define your styles
+// Define styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor: '#2c3e50',
   },
 });
 
-//make this component available to the app
+// Make this component available to the app
 export default App;
---
+++ SplashScreen.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { View, Image, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const SplashScreen: FC = () => {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <StatusBar hidden={true} />
+                <Image style={{ width: '100%', height: '100%', resizeMode: 'stretch', }} source={require('../IdentityCard/assets/splashScreen.jpg')} />
+            </View>
+        </SafeAreaView>
+    );
+};
+export default SplashScreen;
